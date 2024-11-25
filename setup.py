c3bcd663eaea113ab5c43cba5baedfbd43515fbf
GMongelos <gaston.mong@hotmail.com>
2024-11-25 10:25:11 -0300

    Arreglo bug de imagenes que no las tomaba el ejecutable final

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from cx_Freeze import setup, Executable
 
 
-executables = [Executable("main_qt.py", target_name="validador.exe", base="Win32GUI", icon="src/resources/img/bbva.ico")]
+executables = [
+    Executable("main_qt.py", target_name="validador.exe", base="Win32GUI", icon="src/resources/img/bbva.ico"),
+
+]
 
 buildOptions = {
     'packages': ['tkcalendar', 'requests', 'PySide6', 'src.controlm'],
     'excludes': [],
     'include_files': [
-        ('src/resources/img/bbva.ico', 'src/img/bbva.ico'),
-        ('src/resources/img/logo.png', 'src/img/logo.png'),
-        ('src/resources/img/logo_2.png', 'src/img/logo_2.png'),
-        ('src/resources/img/im_bbva.png', 'src/img/im_bbva.png'),
+        ('src/resources/img/bbva.ico', 'src/resources/img/bbva.ico'),
+        ('src/resources/img/logo.png', 'src/resources/img/logo.png'),
+        ('src/resources/img/logo_2.png', 'src/resources/img/logo_2.png'),
+        ('src/resources/img/im_bbva.png', 'src/resources/img/im_bbva.png'),
+        ('src/resources/img/help.png', 'src/resources/img/help.png'),
         ('src/resources/fechas_nolaborables.json', 'src/fechas_nolaborables.json')
     ]
 }
